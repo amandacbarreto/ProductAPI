6c5186a62f9948cfc5b897db067103ffa9517f72
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-09-26 09:48:49 -0300

    Service layer, component registration

--- src/main/java/com/product/productapi/repositories/UserRepository.java
+++ src/main/java/com/product/productapi/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.product.productapi.repositories;
 
 import com.product.productapi.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
